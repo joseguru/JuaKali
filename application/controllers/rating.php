53fa231a88471b71bb04f9cd414c622a0a6915ed
mogetutu <imogetutu@gmail.com>
2012-11-25 14:45:17 +0300

    Added rating

---
+++ application/controllers/rating.php
@@ -0,0 +1,37 @@
+<?php
+
+class Rating_Controller extends Base_Controller
+{
+
+    public $restful = true;
+
+    public function get_index()
+    {
+        # code...
+    }
+
+    public function get_show($id)
+    {
+        # code...
+    }
+
+    public function post_create()
+    {
+        Rating::
+    }
+
+    public function post_update($id)
+    {
+        # code...
+    }
+
+    public function get_edit($id)
+    {
+        # code...
+    }
+
+    public function get_destroy($id)
+    {
+        # code...
+    }
+}
