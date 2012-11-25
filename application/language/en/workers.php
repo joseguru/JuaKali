dbb5d01c84a10ac8b915ad772270484db6bd7035
mogetutu <imogetutu@gmail.com>
2012-11-25 14:45:29 +0300

    Added workers language file

---
+++ application/language/en/workers.php
@@ -0,0 +1,24 @@
+<?php
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Workers Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the users class to build
+    | the user views labels. You're free to change them to anything you want.
+    | If you come up with something more exciting, let us know.
+    |
+    */
+
+   'worker' => 'Worker',
+   'workers' => 'Workers',
+   'profile' => 'Profile',
+   'new_worker' => 'New Worker',
+   'update_worker' => 'Update Worker',
+   'deleted' => 'Worker Deleted.',
+   'added' => 'Worker Added.',
+   'updated' => 'Worker Updated.',
+   'welcome' => 'Welcome, :name',
+);
