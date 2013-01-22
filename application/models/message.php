6a5f16b81c5f8f5a955cd7ae0c6a8dc9c06850c5
mogetutu <imogetutu@gmail.com>
2013-01-23 01:11:54 +0300

    Updated Message Model

--- application/models/message.php
+++ application/models/message.php
@@ -3,15 +3,10 @@
 class Message extends Eloquent
 {
     public static $timestamps = true;
-    public $includes = array('user','worker');
+    public $includes = array('user');
 
     public function user()
     {
         return $this->belongs_to('User');
-    }
-
-    public function worker()
-    {
-        return $this->belongs_to('Worker');
     }
 }
