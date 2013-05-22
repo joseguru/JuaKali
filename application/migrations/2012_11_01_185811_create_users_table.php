18860e327ba68808fb5c83b197452de140dd71f9
mogetutu <imogetutu@gmail.com>
2013-05-22 14:27:36 +0300

    updated create users migration

--- application/migrations/2012_11_01_185811_create_users_table.php
+++ application/migrations/2012_11_01_185811_create_users_table.php
@@ -2,29 +2,32 @@
 
 class Create_Users_Table {
 
-	public function up()
+    public function up()
     {
-		Schema::create('users', function($table) {
-			$table->increments('id');
-			$table->string('name');
-			$table->string('email')->unique();
-			$table->string('password');
+        Schema::create('users', function($table) {
+            $table->increments('id');
+            $table->string('email')->unique();
+            $table->string('name');
             $table->string('username');
-			$table->timestamps();
+            $table->string('password');
+            $table->string('bio')->nullable();
+            $table->timestamps();
         });
 
         DB::table('users')->insert(array(
-            'username' => 'Judith',
+            'username' => 'admin',
             'email'    => 'admin@getwork.com',
             'name'     => 'Administrator',
-            'password' => Hash::make('password')
+            'password' => Hash::make('password'),
+            'created_at' => new DateTime,
+            'updated_at' => new DateTime
         ));
 
     }
 
-	public function down()
+    public function down()
     {
-		Schema::drop('users');
+        Schema::drop('users');
 
     }
 
